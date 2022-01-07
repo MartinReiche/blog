9c0a7edbd600ec64edb9458f878eca3852471dd0
Martin Reiche <martin@reiche.dev>
2022-01-07 16:39:33 +0100

    Added Typescript support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+require('ts-node').register({
+  compilerOptions: {
+    module: 'commonjs',
+    target: 'es2017',
+  },
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
