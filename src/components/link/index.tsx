d6e3a7cb6ef6079f32b931d674865221eb9f1322
Martin Reiche <martin@reiche.dev>
2022-01-13 06:55:32 +0100

    Fixed Link props and pruned Logo attributes

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -1,28 +1,30 @@
 import * as React from 'react';
-import {Link as GatsbyThemeLink } from 'gatsby-theme-material-ui';
+import {Link as GatsbyThemeLink} from 'gatsby-theme-material-ui';
 import {useI18next} from "gatsby-plugin-react-i18next";
 import PropTypes, {InferProps} from "prop-types";
 
 
 function Link(props: InferProps<typeof Link.propTypes>) {
-    const { language, defaultLanguage } = useI18next();
-    const { children, to} = props;
-    let path = language === defaultLanguage ? to : `/${language}${to}`;
+    const {language, defaultLanguage} = useI18next();
+    const {children, to} = props;
 
+    let path = language === defaultLanguage ? to : `/${language}${to}`;
     // first remove all trailing slashes than add excatly one
     path.replace(/\/+$/, '') + "/";
 
+
     return (
-        <GatsbyThemeLink underline="none" {...props} to={path}>
+        <GatsbyThemeLink underline="none" {...props as any} to={path}>
             {children}
         </GatsbyThemeLink>
     )
 }
 
+
 Link.propTypes = {
+    children: PropTypes.node.isRequired,
+    ...GatsbyThemeLink.propTypes,
     to: PropTypes.string.isRequired,
-    children: PropTypes.object,
-    style: GatsbyThemeLink.propTypes
 }
 
 export default Link;
