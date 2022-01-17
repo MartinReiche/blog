01a980e2b051f8826f67b8dafbbae5a1a2c4e82f
Martin Reiche <martin@reiche.dev>
2022-01-17 11:55:12 +0100

    Added Firebase AppCheck

--- src/utils/getFirebase.ts
+++ src/utils/getFirebase.ts
@@ -1,6 +1,7 @@
 import {getApps, getApp, initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 import {getAuth} from 'firebase/auth';
+import {initializeAppCheck, ReCaptchaV3Provider} from 'firebase/app-check';
 
 export function getFirebase() {
     if (getApps().length === 0) {
@@ -14,6 +15,22 @@ export function getFirebase() {
             storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
             appId: process.env.GATSBY_FIREBASE_APP_ID
         });
+
+        // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+        // key is the counterpart to the secret key you set in the Firebase console.
+        if (process.env.GATSBY_FIREBASE_APP_CHECK_DEBUG_MODE === "true") {
+            console.log("Iniatializing Firebase AppCheck in Debug Mode");
+            // @ts-ignore
+            self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+        } else {
+            console.log("Iniatializing Firebase AppCheck in Production Mode");
+        }
+
+        initializeAppCheck(app, {
+            provider: new ReCaptchaV3Provider(process.env.GATSBY_FIREBASE_RECAPTCHA_KEY as string),
+            isTokenAutoRefreshEnabled: true
+        });
+
         return {app, auth: getAuth(app), db: getFirestore(app)};
     } else {
         const app = getApp();
