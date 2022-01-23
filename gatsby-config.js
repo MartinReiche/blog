0183d9e6da8d53b9f94da8a16855fd8d626410df
Martin Reiche <martin@reiche.dev>
2022-01-23 14:59:03 +0100

    Updated favicon and made layout padding responsive

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,7 +107,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+        icon: `src/images/logo_bright.svg`, // This path is relative to the root of the site.
       },
     },
     {
