a66e5151f7b4c9de0d5ab56e9683fcfb4673f191
Martin Reiche <martin@reiche.dev>
2022-01-27 21:03:49 +0100

    lazy load all firebase functions in auth listener

--- src/components/auth/authProvider.tsx
+++ src/components/auth/authProvider.tsx
@@ -1,21 +1,17 @@
 import * as React from "react";
 import PropTypes, {InferProps} from "prop-types";
-import getFirebase from "../../utils/getFirebase";
-import {getAuth} from 'firebase/auth';
 import {Dispatch, SetStateAction} from "react";
 
 type User = {
     isAuthenticated: boolean
     isAdmin: boolean
-    uid?: string
-    displayName?: string
-    isLoading: boolean
+    listening?: boolean
 }
 
 const defaultUser: User = {
     isAdmin: false,
     isAuthenticated: false,
-    isLoading: true
+    listening: false
 }
 
 const AuthContext = React.createContext<{ user: User, setUser: Dispatch<SetStateAction<User>>}>({
@@ -28,29 +24,33 @@ export const useAuth = () => React.useContext(AuthContext);
 export function AuthProvider({children}: InferProps<typeof AuthProvider.propTypes>) {
     const [user, setUser] = React.useState(defaultUser);
 
+    // when the app loads
     React.useEffect(() => {
-        const auth = getAuth(getFirebase());
-
-
-        const cancelAuthListener = auth
-            .onAuthStateChanged(async function(user) {
-                if (user) {
-                    const { claims } = await user.getIdTokenResult();
-                    setUser({
-                        isAuthenticated: true,
-                        isAdmin: !!claims.admin || false,
-                        uid: user.uid,
-                        displayName: user.displayName || '',
-                        isLoading: false
-                    });
-                } else {
-                    setUser({ ...defaultUser, isLoading: false });
-                }
-            });
-        return function cleanup() {
-            cancelAuthListener();
-        }
+        // retrieve the user object and if there is one, load the whole firebase stuff and check auth status
+        const user: User = JSON.parse(localStorage.getItem('user') as string);
+        if (user?.isAuthenticated) checkAuthentication();
     },[])
+
+    React.useEffect(() => {
+        if (user.isAuthenticated && !user.listening) checkAuthentication();
+    },[user])
+
+    const checkAuthentication = async () => {
+        const {getFirebase} = await import('../../utils/getFirebase');
+        const {getAuth, onAuthStateChanged, getIdTokenResult} = await import('firebase/auth');
+        const auth = getAuth(getFirebase());
+        onAuthStateChanged(auth,async (user) => {
+            if (user) {
+                const idTokenResult = await getIdTokenResult(user);
+                const userState = {isAuthenticated: true, isAdmin: !!idTokenResult.claims.admin};
+                setUser({...userState, listening: true});
+                localStorage.setItem('user', JSON.stringify(userState));
+            } else {
+                setUser({...defaultUser, listening: true});
+                localStorage.setItem('user', JSON.stringify({ isAuthenticated: false, isAdmin: false}));
+            }
+        })
+    }
 
     return (
         <AuthContext.Provider value={{user, setUser}}>
